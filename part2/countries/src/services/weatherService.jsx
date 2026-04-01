4409b76b98a21f8aeb1b6b40e1864028aeadd463
Ivan Martynov <ivan.a.martynov@gmail.com>
2026-04-01 10:57:46 +0300

    update weather api key naming

--- part2/countries/src/services/weatherService.jsx
+++ part2/countries/src/services/weatherService.jsx
@@ -2,16 +2,16 @@ import axios from "axios";
 
 const _getResponseData = (request) => request.then((response) => response.data);
 
-const api_key = import.meta.env.VITE_SOME_KEY;
+const api_key = import.meta.env.VITE_WEATHER_API_KEY;
 
 const current = async ({ city }) =>
-    _getResponseData(
-        axios.get(
-            `http://api.weatherapi.com/v1/current.json` +
-                `?key=${api_key}&q=${city}&aqi=no`
-        )
-    );
+  _getResponseData(
+    axios.get(
+      `http://api.weatherapi.com/v1/current.json` +
+        `?key=${api_key}&q=${city}&aqi=no`,
+    ),
+  );
 
 export default {
-    current,
+  current,
 };
